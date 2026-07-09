c757519045ea5daad8bafd32a88790abb6c57a2e
Ambrose Bonnaire-Sergeant <ambrose@ambrosebs.com>
2026-07-08 23:41:41 -0700

    use bit-and correctly

--- subtide/synth/src/subtide/examples/timing/internal_sequencer.clj
+++ subtide/synth/src/subtide/examples/timing/internal_sequencer.clj
@@ -66,9 +66,9 @@
   [buf 0 rate 1 out-bus 0 beat-num 0 sequencer 0 amp 1]
   (let [cnt      (in:kr beat-cnt-bus)
         beat-trg (in:kr beat-trg-bus)
-        bar-trg  (and (buf-rd:kr 1 sequencer cnt)
-                      (= beat-num (mod cnt 8))
-                      beat-trg)
+        bar-trg  (bit-and (buf-rd:kr 1 sequencer cnt)
+                          (= beat-num (mod cnt 8))
+                          beat-trg)
         vol      (set-reset-ff bar-trg)]
     (out
      out-bus (* vol
